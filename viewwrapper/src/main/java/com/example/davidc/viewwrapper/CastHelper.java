53a377129123a976f6bc80cda85f7e3708ff2fbe
David Cryer <davidc@apadmi.com>
2017-03-27 11:35:50 +0100

    add uimodel class rename view wrapper to ui wrapper

--- viewwrapper/src/main/java/com/example/davidc/viewwrapper/CastHelper.java
+++ viewwrapper/src/main/java/com/example/davidc/viewwrapper/CastHelper.java
@@ -6,22 +6,22 @@ import android.content.Context;
 
 class CastHelper {
 
-    static <R extends ViewWrapperRepository> R viewWrapperRepositoryFromFactory(final Activity activity) {
+    static <R extends UiWrapperRepository> R repositoryFromFactory(final Activity activity) {
         final Application application = activity.getApplication();
         try {
             //noinspection unchecked
-            return ((ViewWrapperRepositoryFactory<R>) application).create();
+            return ((UiWrapperRepositoryFactory<R>) application).create();
         } catch (ClassCastException cce) {
-            throw new ClassCastException(application.getClass().getSimpleName() + " must implement ViewWrapperRepositoryFactory, with generic type of ViewWrapperRepository subclass");
+            throw new ClassCastException(application.getClass().getSimpleName() + " must implement UiWrapperRepositoryFactory, with generic type of UiWrapperRepository subclass");
         }
     }
 
-    static <R extends ViewWrapperRepository> ViewWrapperRepositoryProvider<R> viewWrapperRepositoryProvider(final Context context) {
+    static <R extends UiWrapperRepository> UiWrapperRepositoryProvider<R> repositoryProvider(final Context context) {
         try {
             //noinspection unchecked
-            return (ViewWrapperRepositoryProvider<R>) context;
+            return (UiWrapperRepositoryProvider<R>) context;
         } catch (ClassCastException cce) {
-            throw new ClassCastException(context.getClass().getSimpleName() + " must implement ViewWrapperRepositoryFactory, with generic type of ViewWrapperRepository subclass");
+            throw new ClassCastException(context.getClass().getSimpleName() + " must implement UiWrapperRepositoryProvider, with generic type of UiWrapperRepository subclass");
         }
     }
 }
