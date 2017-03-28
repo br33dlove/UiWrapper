811bc630fc70eaf83ac1d599e1ba5a6d42a20622
David Cryer <davidc@apadmi.com>
2017-03-28 12:04:12 +0100

    add basic activity to module

--- uiwrapper/src/main/java/com/example/davidc/uiwrapper/FragmentManagerHelper.java
+++ uiwrapper/src/main/java/com/example/davidc/uiwrapper/FragmentManagerHelper.java
@@ -1,9 +1,17 @@
 package com.example.davidc.uiwrapper;
 
+import android.support.annotation.IdRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 
 class FragmentManagerHelper {
+
+    static boolean noFragmentBoundToView(
+        final FragmentManager fragmentManager,
+        @IdRes final int view
+    ) {
+        return fragmentManager.findFragmentById(view) == null;
+    }
 
     static boolean hasFragment(final FragmentManager fm, final String tag) {
         return fragment(fm, tag) != null;
@@ -13,7 +21,33 @@ class FragmentManagerHelper {
         fm.beginTransaction().add(fragment, tag).commit();
     }
 
+    static void addFragment(
+            final FragmentManager fragmentManager,
+            final Fragment fragment,
+            @IdRes final int view
+    ) {
+        fragmentManager
+                .beginTransaction()
+                .add(view, fragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
     static Fragment fragment(final FragmentManager fm, final String tag) {
         return fm.findFragmentByTag(tag);
+    }
+
+    static boolean hasMoreThanOneNonRetainedFragment(final FragmentManager fragmentManager) {
+        return nonRetainedFragmentCount(fragmentManager) > 1;
+    }
+
+    private static int nonRetainedFragmentCount(final FragmentManager fragmentManager) {
+        int nonRetainedFragmentCount = 0;
+        for (final Fragment fragment : fragmentManager.getFragments()) {
+            if (fragment != null && !fragment.getRetainInstance()) {
+                nonRetainedFragmentCount++;
+            }
+        }
+        return nonRetainedFragmentCount;
     }
 }
---
+++ uiwrapper/src/main/java/com/example/davidc/uiwrapper/SingleContentContainerWithAppBarActivity.java
@@ -0,0 +1,59 @@
+package com.example.davidc.uiwrapper;
+
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+
+public abstract class SingleContentContainerWithAppBarActivity<Repo extends UiWrapperRepository> extends UiWrapperRepositoryActivity<Repo> {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_bar_with_content);
+        setupToolbar();
+        addExampleFragment();
+    }
+
+    private void setupToolbar() {
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            setupActionBar(actionBar);
+        }
+    }
+
+    protected abstract void setupActionBar(final ActionBar actionBar);
+
+    private void addExampleFragment() {
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        if (FragmentManagerHelper.noFragmentBoundToView(fragmentManager, getContentFragmentViewContainer())) {
+            FragmentManagerHelper.addFragment(fragmentManager, initialFragment(), getContentFragmentViewContainer());
+        }
+    }
+
+    protected abstract Fragment initialFragment();
+
+    @IdRes
+    private int getContentFragmentViewContainer() {
+        return R.id.content;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        if (FragmentManagerHelper.hasMoreThanOneNonRetainedFragment(fragmentManager)) {
+            super.onBackPressed();
+            return;
+        }
+        finish();
+    }
+}
