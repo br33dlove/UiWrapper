6b83554682bb4e9c2667b0c72e890e6260ce5fd7
David <davidcryer@Davids-MBP.home>
2017-04-27 19:03:28 +0100

    refactor

--- uiwrapper/src/main/java/com/davidc/uiwrapper/ArgChecker.java
+++ uiwrapper/src/main/java/com/davidc/uiwrapper/ArgChecker.java
@@ -2,13 +2,13 @@ package com.davidc.uiwrapper;
 
 final class ArgChecker {
 
-    static void notNull(final Object o) {
+    static void notNull(final Object o, final String varName) {
         if (o == null) {
-            throw new IllegalArgumentException(defaultExceptionMessage(o));
+            throw new IllegalArgumentException(defaultExceptionMessage(varName));
         }
     }
 
-    private static String defaultExceptionMessage(final Object o) {
-        return String.format("%1$s cannot be null", o.getClass().getSimpleName());
+    private static String defaultExceptionMessage(final String varName) {
+        return String.format("%1$s cannot be null", varName);
     }
 }
