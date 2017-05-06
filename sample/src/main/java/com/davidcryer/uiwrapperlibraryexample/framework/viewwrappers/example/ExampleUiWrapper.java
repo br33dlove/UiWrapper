7e05b1353a4d0959c4b4ef267fbf03d1620f422f
David Cryer <davecryer.dc@gmail.com>
2017-05-06 16:02:49 +0100

    move stuff

--- sample/src/main/java/com/davidcryer/uiwrapperlibraryexample/framework/viewwrappers/example/ExampleUiWrapper.java
+++ sample/src/main/java/com/davidcryer/uiwrapperlibraryexample/framework/viewwrappers/example/ExampleUiWrapper.java
@@ -29,19 +29,6 @@ public class ExampleUiWrapper extends UiWrapper<ExampleUi, ExampleUi.Listener, E
         return uiModel == null ? newInstance(resource, uiModelFactory) : new ExampleUiWrapper(resource, uiModel);
     }
 
-    private final Resource.Listener resourceListener = new Resource.Listener() {
-        @Override
-        public void listenerCount(int count) {
-            uiModel().showResourceListenersCount(ui(), count);
-        }
-    };
-
-    @Override
-    protected void registerResources() {
-        super.registerResources();
-        resource.register(resourceListener);
-    }
-
     @Override
     protected ExampleUi.Listener uiListener() {
         return new ExampleUi.Listener() {
@@ -51,6 +38,19 @@ public class ExampleUiWrapper extends UiWrapper<ExampleUi, ExampleUi.Listener, E
             }
         };
     }
+
+    @Override
+    protected void registerResources() {
+        super.registerResources();
+        resource.register(resourceListener);
+    }
+
+    private final Resource.Listener resourceListener = new Resource.Listener() {
+        @Override
+        public void listenerCount(int count) {
+            uiModel().showResourceListenersCount(ui(), count);
+        }
+    };
 
     @Override
     protected void unregisterResources() {
