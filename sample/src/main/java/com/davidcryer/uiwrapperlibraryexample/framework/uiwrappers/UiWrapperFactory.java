b94396c87106d78c9016eb00366b1ce60e1c0095
David Cryer <davecryer.dc@gmail.com>
2018-01-25 23:41:20 +0000

    use helper method for ui wrapper creation in sample

--- sample/src/main/java/com/davidcryer/uiwrapperlibraryexample/framework/uiwrappers/UiWrapperFactory.java
+++ sample/src/main/java/com/davidcryer/uiwrapperlibraryexample/framework/uiwrappers/UiWrapperFactory.java
@@ -3,6 +3,7 @@ package com.davidcryer.uiwrapperlibraryexample.framework.uiwrappers;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.davidc.uiwrapper.UiWrapperInitializer;
 import com.davidcryer.uiwrapperlibraryexample.framework.uiwrappers.example.ExampleUiWrapper;
 import com.davidcryer.uiwrapperlibraryexample.framework.uiwrappers.example.viewmodel.ExampleUiModelFactory;
 import com.davidcryer.uiwrapperlibraryexample.model.ResourceRepository;
@@ -17,8 +18,10 @@ public class UiWrapperFactory {
     }
 
     public ExampleUiWrapper createExampleUiWrapper(@Nullable final Bundle savedState) {
-        return savedState == null
-                ? ExampleUiWrapper.newInstance(resourceRepository.create(), exampleUiModelFactory)
-                : ExampleUiWrapper.savedElseNewInstance(resourceRepository.create(), exampleUiModelFactory, savedState);
+        return UiWrapperInitializer.from(savedState, () -> {
+            return ExampleUiWrapper.newInstance(resourceRepository.create(), exampleUiModelFactory);
+        }, nonNullSavedState -> {
+            return ExampleUiWrapper.savedElseNewInstance(resourceRepository.create(), exampleUiModelFactory, savedState);
+        });
     }
 }
